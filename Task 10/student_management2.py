5a31873639d32a986bb0834931b1c4e016c925fe
JoannaGeorg <joegeorge231@gmail.com>
2026-08-07 16:44:29 +0400

    Changed the variable names in student management.

--- Task 10/student_management2.py
+++ Task 10/student_management2.py
@@ -1,39 +1,38 @@
-from student import Student
 from mangement import StudentManagement
 
 
-class_a = StudentManagement()
+classA = StudentManagement()
 print("STUDENT MANAGEMENT PROGRAM")
 
 cont = True
 while cont:
   input("\nPress Enter to Continue.")
-  op = input("\n1. Add a Student\n2. Print the list of Students\n3. Search for a Student\n4. Get the Average Marks\n5. Get Grade of a Student\n6. Update Student Information\n7. Delete a Student\n\nE to Exit\n\nPlease Enter the index of the function to perform: ")
-  if op == '1':
+  option = input("\n1. Add a Student\n2. Print the list of Students\n3. Search for a Student\n4. Get the Average Marks\n5. Get Grade of a Student\n6. Update Student Information\n7. Delete a Student\n\nE to Exit\n\nPlease Enter the index of the function to perform: ")
+  if option == '1':
     name = input("\nEnter the name of the student: ")
     marks = float(input("Enter the marks of the student: "))
-    class_a.addStudent(name, marks)
-  elif op == '2':
-    class_a.viewStudents()
-  elif op == '3':
+    classA.addStudent(name, marks)
+  elif option == '2':
+    classA.viewStudents()
+  elif option == '3':
     name = input("\nEnter the name of the student: ")
-    student = class_a.searchStudent(name)
+    student = classA.searchStudent(name)
     if student:
       print(student)
-  elif op == '4':
-    print(f"\nThe average of the class = {class_a.getAverage()}")
-  elif op == '5':
+  elif option == '4':
+    print(f"\nThe average of the class = {classA.getAverage()}")
+  elif option == '5':
     name = input("\nEnter the name of the student: ")
-    grade = class_a.getGrade(name)
+    grade = classA.getGrade(name)
     if grade:
       print(f"{name} => Grade: {grade}")
-  elif op == '6':
+  elif option == '6':
     name = input("\nEnter the name of the student: ")
-    class_a.updateStudent(name)
-  elif op == '7':
+    classA.updateStudent(name)
+  elif option == '7':
     name = input("\nEnter the name of the student: ")
-    class_a.deleteStudent(name)
-  elif op == 'E' or op == 'e':
+    classA.deleteStudent(name)
+  elif option == 'E' or option == 'e':
     print("Goodbye.")
     cont = False
   else:
